4dcc6c4f0e25ebae016b90a652fbc142a69480ca
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2020-04-19 17:18:01 +0900

    implement classify_triangle()

--- 20200419/test.py
+++ 20200419/test.py
@@ -1,6 +1,7 @@
 import unittest
 from src.between import between
 from src.inc_digits import inc_digits
+from src.classify_triangle import classify_triangle
 
 
 class TestBetween(unittest.TestCase):
@@ -34,6 +35,30 @@ class TestIncDigits(unittest.TestCase):
 
         for s, n, expected in zip(ss, ns, expecteds):
             self.assertEqual(expected, inc_digits(s, n))
+
+
+class TestClassifyTriangle(unittest.TestCase):
+    def test_classify_triangle(self):
+        args = [
+            [3, 6, 5],
+            [3, 4, 5],
+            [4, 3, 3],
+            [1, 1, 3],
+            [1, 2, 1],
+            [3, 2, 1]
+        ]
+        expecteds = [
+            3,
+            2,
+            1,
+            0,
+            0,
+            0
+        ]
+
+        for arg, expected in zip(args, expecteds):
+            a, b, c = arg
+            self.assertEqual(expected, classify_triangle(a, b, c))
 
 
 if __name__ == "__main__":
