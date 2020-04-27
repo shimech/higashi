851b4d93e68001271a4bbb20574a93a05971eec6
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2020-04-27 09:30:56 +0900

    reconstruction of directories

--- src/test.py
+++ src/test.py
@@ -1,7 +1,67 @@
 import unittest
-from src.tuple_list import tuple_list
-from src.letter_n_gram_list import letter_n_gram_list
-from src.inc_freq import inc_freq
+from ex1.classify_triangle import classify_triangle
+from pre2.between import between
+from pre2.inc_digits import inc_digits
+from ex2.tuple_list import tuple_list
+from pre3.letter_n_gram_list import letter_n_gram_list
+from pre3.inc_freq import inc_freq
+
+
+class TestClassifyTriangle(unittest.TestCase):
+    def test_classify_triangle(self):
+        args = [
+            [3, 6, 5],
+            [3, 4, 5],
+            [4, 3, 3],
+            [1, 1, 3],
+            [1, 2, 1],
+            [3, 2, 1]
+        ]
+        expecteds = [
+            3,
+            2,
+            1,
+            0,
+            0,
+            0
+        ]
+
+        for arg, expected in zip(args, expecteds):
+            a, b, c = arg
+            self.assertEqual(expected, classify_triangle(a, b, c))
+
+
+class TestBetween(unittest.TestCase):
+    def test_between(self):
+        lns = [
+            [20, 10, 50, 30, 40],
+            ["a", "b", "c", "d", "e"]
+        ]
+        xs = [
+            10,
+            "e"
+        ]
+        ys = [
+            30,
+            "c"
+        ]
+        expecteds = [
+            [10, 50, 30],
+            ["c", "d", "e"]
+        ]
+
+        for ln, x, y, expected in zip(lns, xs, ys, expecteds):
+            self.assertEqual(expected, between(ln, x, y))
+
+
+class TestIncDigits(unittest.TestCase):
+    def test_inc_digits(self):
+        ss = ["12", "457"]
+        ns = [5, 5]
+        expecteds = ["67", "902"]
+
+        for s, n, expected in zip(ss, ns, expecteds):
+            self.assertEqual(expected, inc_digits(s, n))
 
 
 class TestTupleList(unittest.TestCase):
