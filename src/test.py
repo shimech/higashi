f974774694b6158c25b68fe194fc6eb37ffd08f5
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2020-04-27 10:48:06 +0900

    add test case

--- src/test.py
+++ src/test.py
@@ -149,12 +149,14 @@ class TestTranspose(unittest.TestCase):
         Ms = [
             [[1, 2, 3], [4, 5, 6]],
             [[0]],
-            [[1, 2, 3]]
+            [[1, 2, 3]],
+            [[1], [2], [3], [4]]
         ]
         expecteds = [
             [[1, 4], [2, 5], [3, 6]],
             [[0]],
-            [[1], [2], [3]]
+            [[1], [2], [3]],
+            [[1, 2, 3, 4]]
         ]
 
         for M, expected in zip(Ms, expecteds):
