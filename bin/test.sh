811cded8c7cab93eb29e018e951190472a52d427
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2020-05-19 00:15:02 +0900

    modify shell script

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-pipenv run python ./src/test.py -v
+if [ $# -eq 0 ]; then
+    FILES=./src/*/test.py
+    for file in ./src/*/test.py; do
+        echo "### Testing $file ... ###"
+        pipenv run python $file -v
+    done
+else
+    for dir in "$@"; do
+        file=./src/$dir/test.py
+        if [ -e $file ]; then
+            echo "### Testing $file ... ###"
+            pipenv run python $file -v
+        else
+            echo "$file is NOT found."
+        fi
+    done
+fi
